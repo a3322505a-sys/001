7d9a133acd35d115a70efb03ad4d356b77e6fc47
a3322505a-sys <a3322505a@gmail.com>
2026-08-29 02:10:01 +0800

    Fix dynamic fretboard geometry assertion

--- app/src/test/java/com/a3322505a/guitarlearning/ui/fretboard/FretboardGeometryTest.kt
+++ app/src/test/java/com/a3322505a/guitarlearning/ui/fretboard/FretboardGeometryTest.kt
@@ -7,7 +7,7 @@ class FretboardGeometryTest {
     @Test
     fun stringCentersStayInsideTheCanvasWithEqualDynamicRows() {
         assertEquals(1f / 12f, stringCenterFraction(6), 0.0001f)
-        assertEquals(0.5f, stringCenterFraction(3), 0.0001f)
+        assertEquals(7f / 12f, stringCenterFraction(3), 0.0001f)
         assertEquals(11f / 12f, stringCenterFraction(1), 0.0001f)
     }
 }
